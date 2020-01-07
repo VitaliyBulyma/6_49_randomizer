fe050bd65c7a5616855a69fb055cdfe20387e86a
VitaliyBulyma <55520185+VitaliyBulyma@users.noreply.github.com>
2020-01-06 21:21:09 -0500

    commented out ui

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ var output = document.getElementById("container");
 var topSixOutput = document.getElementById("topSix");
 output.innerHTML= "";
 topSixOutput= "";
-// arrays to hold random numbers 
+// arrays to hold random numbers
 var allNumbersArray =  [];
 var topSix=[];
 
 // call function to get how many random numbers user want to run
-getNumbers(sendValue());
-console.log(allNumbersArray); 
+getNumbers(400);
+console.log(allNumbersArray);
 
 
 // loop to check how many times each number in the range occurs(range 1-49 inclusive)
@@ -22,7 +22,7 @@ for (var z=1; z<50;z++){
 
 
 
-// topSix.sort(function(a, b){return b-a}).slice(0,6); 
+// topSix.sort(function(a, b){return b-a}).slice(0,6);
 
 
 
@@ -32,14 +32,13 @@ for (var z=1; z<50;z++){
  *                                                   *
  *                 FUNCTIONS                         *
  *                                                   *
- *                                                   * 
+ *                                                   *
 ****************************************************/
-
-function sendValue(){
+/*function sendValue(){
     var t = 0;
     t = document.getElementById("input").value;
     return t;
-};//onclick function
+};//onclick function*/
 
 
 
@@ -59,9 +58,9 @@ function getNumbers(x){
     };
 }; // function to return x of random numbers within the range as string
 
- 
+
 //getSixRandNumbers();
-/*function getSixRandNumbers(){ 
+/*function getSixRandNumbers(){
     for (var i=1; i<7; i++){
     output.innerHTML += getRandomIntInclusive(1,49) + "<br />";
     };// prints out 6 random numbers within the range to a div
@@ -69,9 +68,9 @@ function getNumbers(x){
 
 
 
-function getRandomIntInclusive(min, max) { 
+function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     y = Math.floor(Math.random() * (max - min + 1)) + min;
-    return y; 
+    return y;
   };// returns random number in a range inclusive min and max
