a0f2404fb4a2f5fd8dfeb2c8ab0874730216ded1
VitaliyBulyma <55520185+VitaliyBulyma@users.noreply.github.com>
2020-06-13 15:14:13 -0400

    created key:value pairs

--- script.js
+++ script.js
@@ -8,17 +8,22 @@ var allNumbersArray =  [];
 var topSix=[];
 
 // call function to get how many random numbers user want to run
-getNumbers(400);
-console.log(allNumbersArray);
+getNumbers(600);
+// console.log(allNumbersArray);
 
 
 // loop to check how many times each number in the range occurs(range 1-49 inclusive)
+// add numbers to key:value pairs
+var mapArray= new Map();
 for (var z=1; z<50;z++){
     var howManytimes=counterOfInstances(allNumbersArray,z);
+    
+    mapArray.set(z,howManytimes);
+    
     output.innerHTML += z + " occurs " + howManytimes + " times " + "<br />";
-   };
+   }
+console.log(mapArray);
 
-// topSix.sort(function(a, b){return b-a}).slice(0,6);
 
 
 /*****************************************************
@@ -28,37 +33,27 @@ for (var z=1; z<50;z++){
  *                                                   *
 ****************************************************/
 
-
+// Counts how many times value appears in an array
 function counterOfInstances(array,value){
     var n = 0;
     for(i = 0; i < array.length; i++){
-        if(array[i] === value){n++}
+        if(array[i] === value){n++;}
     }
     return n;
-};// Counts how many times value appears in an array
+}
 
-
+// function to return x of random numbers within the range as string
 function getNumbers(x){
     output.innerHTML += "By running " + x + " random numbers in a range from 1 to 49: " + "<br />";
     for (var i=1; i<=x; i++){
         allNumbersArray.push(getRandomIntInclusive(1,49)) ;
-    };
-}; // function to return x of random numbers within the range as string
+    }
+} 
 
-
-
-
+// returns random number in a range inclusive min and max
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     y = Math.floor(Math.random() * (max - min + 1)) + min;
     return y;
-  };// returns random number in a range inclusive min and max
-
-
-  //getSixRandNumbers();
-  /*function getSixRandNumbers(){
-      for (var i=1; i<7; i++){
-      output.innerHTML += getRandomIntInclusive(1,49) + "<br />";
-      };// prints out 6 random numbers within the range to a div
-  };*/
+  }
